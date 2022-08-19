292421f3af505801aada1a782d3046e879f77d33
Mateus Ferreira <mateusferreira0704@gmail.com>
2022-08-18 22:07:22 -0300

    categories

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,7 @@
 class FriendsController < ApplicationController
+  def index
+    @friends = Friend.all
+  end
 
   def new
     @friend = Friend.new
@@ -7,6 +10,10 @@ class FriendsController < ApplicationController
   def create
     @friend = Friend.new(friends_params)
     @friend.user = current_user
+
+    # @product_category = ProductCategory.new(product_categories_params)
+    # @product_category.friend = @friend
+    # @product_category.save
 
     if @friend.save
       redirect_to root_path
@@ -17,5 +24,9 @@ class FriendsController < ApplicationController
 
   def friends_params
     params.require(:friend).permit(:name, :max_price, :birthday, :phone_number)
+  end
+
+  def product_categories_params
+    params.require(:product_category).permit(name: :product_categories)
   end
 end
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,3 +1,5 @@
 class ProductCategory < ApplicationRecord
+  CATEGORIES = ["Video Game", "House", "Cars", "Kitchen"]
+
   belongs_to :friend
 end
