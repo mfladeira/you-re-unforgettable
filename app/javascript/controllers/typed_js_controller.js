ed376f688395f5b6f09f0d29b56db29865282906
leonardo fukuje <leonardo.fukuje@gmail.com>
2022-08-19 01:01:52 -0300

    home finished

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,17 @@
+// app/javascript/controllers/typed_js_controller.js
+import { Controller } from "@hotwired/stimulus"
+// Don't forget to import the NPM package
+import Typed from "typed.js"
+
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["shows that you care.",
+                 "makes people feel special.",
+                 "shows that you are there.",
+                 "connects people in a unique way."],
+      typeSpeed: 80,
+      loop: true
+    });
+  }
+}
