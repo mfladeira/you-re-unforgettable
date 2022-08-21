75d96dc13dc8c3cf751063d2e7cc85b7d6fa3489
leonardo fukuje <leonardo.fukuje@gmail.com>
2022-08-20 23:41:49 -0300

    added new friend page

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,5 +1,7 @@
 class ProductCategory < ApplicationRecord
-  CATEGORIES = ["Video Game", "House", "Cars", "Kitchen"]
+  CATEGORIES = ["Automotive & Powersports", "Beauty", "Books", "Cell Phones & Accessories",
+                 "Electronics", "Home & Garden", "Musical Instruments", "Personal Computers",
+                 "Sports", "Tools", "Video Games", "Watches"]
 
   belongs_to :friend
 end
