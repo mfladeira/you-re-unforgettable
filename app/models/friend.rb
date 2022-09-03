d4354363661328ae13b46707f3886713fc53325c
Mateus Ferreira <mateusferreira0704@gmail.com>
2022-09-03 17:10:03 -0300

    model friend changed

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,6 +2,4 @@ class Friend < ApplicationRecord
   CATEGORIES = ["Beauty", "Eletronics", "Home & Garden", "Sports", "Watches"]
   belongs_to :user
   has_one_attached :avatar
-  has_many :gifts, dependent: :destroy
-
 end
