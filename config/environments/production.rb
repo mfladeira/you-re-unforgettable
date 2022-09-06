0f0e6e7277ebde938fd35f5ec2b7380b91f9b9ee
Mateus Ferreira <mateusferreira0704@gmail.com>
2022-09-06 20:31:23 -0300

    styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "you-re-unforgettable.me" }
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.serve_static_assets = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
