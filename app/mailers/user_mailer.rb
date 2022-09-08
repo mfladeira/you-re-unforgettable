d669330147c0843226273240bdc90c74bb159de5
Mateus Ferreira <mateusferreira0704@gmail.com>
2022-09-08 15:48:08 -0300

    last change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,6 +9,6 @@ class UserMailer < ApplicationMailer
     @user = user
     @friend = Friend.find(friend_id)
     @gifts = Gift.where("price < ? and category = ?", 200.to_d, @friend.product_category).first(3)
-    mail to: user.email, subject: "Worked"
+    mail to: user.email, subject: "Birthday of #{@friend.name} is in 7 days"
   end
 end
